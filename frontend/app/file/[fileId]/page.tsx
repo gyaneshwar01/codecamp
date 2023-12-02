cd879658e855fdb007d6fcb5552b1c96675a0368
Gyaneshwar Sah <gyaneshwar585@gmail.com>
2023-12-02 16:14:46 +0545

    Initial Pdf UI

--- frontend/app/file/[fileId]/page.tsx
+++ frontend/app/file/[fileId]/page.tsx
@@ -12,8 +12,10 @@ const SpecificPage = ({ params }: ISpecificPageProps) => {
 
   return (
     <div className="flex flex-row">
-      <div className="flex-[3] bg-black h-screen">
-        <PdfReader fileId={fileId} />
+      <div className="flex-[3] h-screen">
+        <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+          <PdfReader fileId={fileId} />
+        </div>
       </div>
       <div className="flex-[2] h-screen">
         <ChatBox />
