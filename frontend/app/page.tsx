fec947fad7bb8fbe5a3873ee04b066d83c238e4c
Aadesh Manandhar <aadeshmanandhar14646@gmail.com>
2023-12-02 09:19:02 +0545

    test commit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import ChatBox from "@/components/chat/ChatBox";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello, World</h1>
       <Button variant={"destructive"}>Click Me!</Button>
+      <ChatBox />
     </div>
   );
 }
---
+++ frontend/components/chat/ChatBox.tsx
@@ -0,0 +1,45 @@
+import InputBox from "./InputBox";
+import MessageBox from "./MessageBox";
+import type { IMessage } from "@/models/chatbox";
+import { nanoid } from "nanoid";
+
+const DUMMY_MESSAGES: IMessage[] = [
+  { text: "Hello 1", time: new Date("11/12/2023"), sender: "user", id: nanoid() },
+  {
+    text: "It's a good day 2",
+    time: new Date("12/12/2023"),
+    sender: "app",
+    id: nanoid(),
+  },
+  {
+    text: "It's a good day 3",
+    time: new Date("12/12/2023"),
+    sender: "app",
+    id: nanoid(),
+  },
+  {
+    text: "last text 4",
+    time: new Date("5/12/2023"),
+    sender: "user",
+    id: nanoid(),
+  },
+  {
+    text: "last text2 5",
+    time: new Date("5/12/2023"),
+    sender: "user",
+    id: nanoid(),
+  },
+];
+
+interface IProps {}
+
+const ChatBox: React.FC<IProps> = () => {
+  return (
+    <div className="flex flex-col">
+      <MessageBox messages={DUMMY_MESSAGES.reverse()} />
+      <InputBox />
+    </div>
+  );
+};
+
+export default ChatBox;
