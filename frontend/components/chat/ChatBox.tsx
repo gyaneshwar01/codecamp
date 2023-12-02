93df9d1f2ff70f230cfffa8c109675846d37bb8e
Aadesh Manandhar <aadeshmanandhar14646@gmail.com>
2023-12-02 13:39:29 +0545

    chat box complete

--- frontend/components/chat/ChatBox.tsx
+++ frontend/components/chat/ChatBox.tsx
@@ -5,35 +5,6 @@ import { useQuery } from "@tanstack/react-query";
 import InputBox from "./InputBox";
 import MessageBox from "./MessageBox";
 import type { IMessage } from "@/models/chatbox";
-import { nanoid } from "nanoid";
-
-const DUMMY_MESSAGES: IMessage[] = [
-  { text: "Hello 1", time: "11/12/2023", sender: "user", id: nanoid() },
-  {
-    text: "It's a good day 2",
-    time: "12/12/2023",
-    sender: "app",
-    id: nanoid(),
-  },
-  {
-    text: "It's a good day 3",
-    time: "12/12/2023",
-    sender: "app",
-    id: nanoid(),
-  },
-  {
-    text: "last text 4",
-    time: "5/12/2023",
-    sender: "user",
-    id: nanoid(),
-  },
-  {
-    text: "last text2 5",
-    time: "5/12/2023",
-    sender: "user",
-    id: nanoid(),
-  },
-];
 
 interface IProps {}
 
@@ -41,13 +12,25 @@ const ChatBox: React.FC<IProps> = () => {
   const { data } = useQuery<IMessage[]>({
     queryKey: ["chats"],
     queryFn: async () => {
-      return [];
+      const response = await fetch("/api/chats");
+
+      if (!response.ok){
+        return [];
+      }
+
+      const data = await response.json();
+
+  
+
+      data.reverse();
+
+      return data;
     }
   });
 
   return (
     <div className="flex flex-col h-full bg-primary-foreground">
-      <MessageBox messages={DUMMY_MESSAGES.reverse()} />
+      <MessageBox messages={(data || [])} />
       <InputBox />
     </div>
   );
